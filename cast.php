e631930313043d2e5d00a5ed09fbded54ac20aed
Austin Eng <213reeses@gmail.com>
2013-09-02 23:52:21 -0400

    [new] added more cast characters

--- cast.php
+++ cast.php
@@ -8,6 +8,8 @@ if(empty($access)) {
     <img id="karissa_bg" class="cast-bg background bg-in" src="images/cast/jaclyn.jpg" />
     <img id="matt_bg" class="cast-bg background" src="images/cast/matthias.jpg" />
     <img id="alex_bg" class="cast-bg background" src="images/cast/saihttam.jpg" />
+    <img id="timmy_bg" class="cast-bg background" src="images/cast/darren.jpg" />
+    <img id="kristen_bg" class="cast-bg background" src="images/cast/heidi.jpg" />
     <div class="row" style="height:100%">
         <div class="col-sm-3" id="sidebar">
             <div class="person_tag active-side" id="karissa">
@@ -18,6 +20,12 @@ if(empty($access)) {
             </div>
             <div class="person_tag" id="alex">
                 Alex Mendelson
+            </div>
+            <div class="person_tag" id="timmy">
+                Timmy Chan
+            </div>
+            <div class="person_tag" id="kristen">
+                Kristen Yu-Um
             </div>
         </div>
         <div class="col-sm-9" style="height:100%">
@@ -42,6 +50,20 @@ if(empty($access)) {
                     <div class="col-sm-12 bio">
                         <p><span class="first-word">Saittham</span> was once an angel named Sabael. He served in heaven alongside Matthias, his closest friend. Then came the Fall. Sabael chose one side; his best friend chose the other. Now, they are locked in a never-ending battle over human souls.</p>
                         <p>All demons know their ultimate fate: eternal damnation in the Lake of Fire. It has been predestined and cannot change. Drained of all hope and joy, Saihttam's only purpose in life is to take it from others. Cunning, strategic, and menacing, he'll do whatever it takes to bring down as many souls as possible. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="actor" id="timmy_a">
+                <div class="row">
+                    <div class="col-sm-12 bio">
+                        <p><span class="first-word">Darren</span> is a website developer in his early 20s, working overtime to provide for his soon-to-be-born child. He's not sure if he can be a good father; he has a hard enough time being a good husband. No one can say he didn't try, even when his attempts fall short of other's expectations. He loves his wife more than anything in the world. Despite having a solid job, he constantly worries that he won't be able to provide for their family.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="actor" id="kristen_a">
+                <div class="row">
+                    <div class="col-sm-12 bio">
+                        <p><span class="first-word">Heidi</span> is the oldest out of six other siblings. She was the first to get married, and now the first to have a child. While she was still dating Darren, she worked as a substitute elementary school teacher. It sparked in her a passion for children; she can't wait to have one of her own. She doesn't understand her husband's worries since there will always be worries in any situation, but she tries her best to calm his fears, reassuring him that everything will be alright. After all, he's not the one going through labor. </p>
                     </div>
                 </div>
             </div>
